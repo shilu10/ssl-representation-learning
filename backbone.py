8f5dd319e176c78646dbcae7efd546d0448aa217
Shilu <shilu842003@gmail.com>
2023-12-13 07:57:02 +0530

    updated main.py and helper.py

--- backbone.py
+++ backbone.py
@@ -365,16 +365,16 @@ class ResNet50(tf.keras.Model):
       return x
 
 
-def simple_cnn(input_shape):
+def simple_cnn(input_shape, width):
   encoder = tf.keras.Sequential(
           [
             tf.keras.layers.Input(shape=(input_shape, input_shape, 3)),
-            tf.keras.layers.Conv2D(args.width, kernel_size=3, strides=2, activation="relu"),
-            tf.keras.layers.Conv2D(args.width, kernel_size=3, strides=2, activation="relu"),
-            tf.keras.layers.Conv2D(args.width, kernel_size=3, strides=2, activation="relu"),
-            tf.keras.layers.Conv2D(args.width, kernel_size=3, strides=2, activation="relu"),
+            tf.keras.layers.Conv2D(width, kernel_size=3, strides=2, activation="relu"),
+            tf.keras.layers.Conv2D(width, kernel_size=3, strides=2, activation="relu"),
+            tf.keras.layers.Conv2D(width, kernel_size=3, strides=2, activation="relu"),
+            tf.keras.layers.Conv2D(width, kernel_size=3, strides=2, activation="relu"),
             tf.keras.layers.Flatten(),
-            tf.keras.layers.Dense(args.width, activation="relu"),
+            tf.keras.layers.Dense(width, activation="relu"),
           ],
           name="encoder",
        )
