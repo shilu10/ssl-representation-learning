032dc4aaa84f1aa7899481d17301feb9da803a50
Shilu <shilu842003@gmail.com>
2023-12-22 07:50:57 +0530

    feat: added alexnet for jigsaw ssl

--- backbone.py
+++ backbone.py
@@ -21,8 +21,9 @@ Adapted from tf.keras.applications.ResNet50. A notable difference is that the
 model here outputs logits while the Keras model outputs probability.
 """
 import functools
-
 import tensorflow as tf
+from typing import Union
+
 
 layers = tf.keras.layers
 
@@ -380,3 +381,188 @@ def simple_cnn(input_shape, width):
        )
 
   return encoder
+
+
+class ConvLayer(tf.keras.layers.Layer):
+  def __init__(self, 
+              kernel_size: tuple = (3, 3), 
+              filters: int = 10, 
+              strides: Union(tuple, None) = (1, 1), 
+              padding: str = "valid",
+              groups=1,
+              use_act: bool = True, 
+              act_type: str = "relu", 
+              use_pooling: bool = True,
+              pooling_type: str = "max",
+              pool_size: tuple = (2, 2),
+              pool_strides: Union(tuple, None) = None,
+              pool_padding: str: "valid",
+              *args, 
+              **kwargs):
+
+    super(ConvLayer, self).__init__(*args, **kwargs)
+
+      # conv
+      self.conv = tf.keras.layers.Conv2D(kernel_size=kernel_size, 
+                                  filters=filters, padding=padding, strides=strides, groups=groups)
+
+      # activation
+      if use_act:
+        self.activation = tf.keras.layers.Activation(act_type)
+
+      # pooling
+      if use_pooling:
+        if pooling_type == "max":
+          self.pooling = tf.keras.layers.MaxPooling2D(pool_size=pool_size, 
+                                              padding=pool_padding, strides=pool_strides)
+
+        elif pooling_type == "avg":
+          self.pooling = tf.keras.layers.AveragePooling2D(pool_size=pool_size, 
+                                              padding=pool_padding, strides=pool_strides)
+
+  def call(self, inputs):
+    x = self.conv(inputs)
+    x = self.activation(x)
+    x = self.pooling(x)
+
+    return x 
+
+
+class LinearLayer(tf.keras.models.Model):
+  def __init__(self, 
+              units: int = 1028, 
+              use_act: bool = True, 
+              act_type: str = 'relu', 
+              use_dropout: bool = True, 
+              dropout_rate: float = 0.5, 
+              *args, **kwargs):
+
+    super(LinearLayer, self).__init__(*args, **kwargs)
+
+    self.linear = tf.keras.layers.Dense(units=units)
+
+    # activaton
+    if use_act:
+      self.activation = tf.keras.layers.Activation(act_type)
+
+    # dropout
+    if use_dropout:
+      self.dropout = tf.keras.layers.Dropout(rate=dropout_rate)
+
+
+  def call(self, inputs: tf.Tensor):
+    x = self.linear(inputs)
+
+    x = self.activation(x)
+    x = self.dropout(x)
+
+    return x 
+
+class AlexNet(tf.keras.models.Model):
+  def __init__(self, n_classes=1000, *args, **kwargs):
+    super(AlexNet, self).__init__(*args, **kwargs)
+
+    self.conv_layer_1 = ConvLayer(kernel_size=11, 
+                              strides=2, 
+                              filters=96,
+                              padding="valid", 
+                              use_act=True, 
+                              act_type='relu', 
+                              use_pooling=True, 
+                              pooling_type='max', 
+                              pool_size=(3, 3), 
+                              pool_strides=(2, 2)
+                        )
+
+    self.conv_layer_2 = ConvLayer(kernel_size=(5, 5), 
+                              groups=2,
+                              filters=256,
+                              padding='same', 
+                              use_act=True, 
+                              act_type='relu', 
+                              use_pooling=True, 
+                              pooling_type='max', 
+                              pool_size=(3, 3), 
+                              pool_strides=(2, 2)
+                        )
+
+    self.conv_layer_3 = ConvLayer(kernel_size=(3, 3), 
+                              filters=384,
+                              padding='same', 
+                              use_act=True, 
+                              act_type='relu', 
+                              use_pooling=False, 
+                        )
+
+    self.conv_layer_4 = ConvLayer(kernel_size=(3, 3),
+                              groups=2,
+                              filters=384, 
+                              padding='same', 
+                              use_act=True, 
+                              act_type='relu', 
+                              use_pooling=False, 
+                        )
+
+    self.conv_layer_5 = ConvLayer(kernel_size=(3, 3),
+                              groups=2,
+                              filters=256, 
+                              padding='same', 
+                              use_act=True, 
+                              act_type='relu', 
+                              use_pooling=True, 
+                              pooling_type="max", 
+                              pool_size=(3, 3), 
+                              pool_strides=(2, 2), 
+                        )
+
+    self.linear_layer_1 = LinearLayer(units=1024, 
+                                use_act=True, 
+                                act_type='relu', 
+                                use_dropout=True, 
+                                dropout_rate=0.5
+                        )
+
+    self.linear_layer_2 = LinearLayer(units=4096, 
+                                use_act=True,
+                                act_type='relu', 
+                                use_dropout=True, 
+                                dropout_rate=0.5
+                        )
+
+    self.flatten = tf.keras.layers.Flatten()
+
+    self.output = tf.keras.layers.Dense(n_classes)
+
+    def call(self, inputs, training=False):
+      # B-batch, T-tile, H-height, W-width, C-channels
+      B, T, H, W, C = inputs.shape 
+
+      inputs = tf.transpose(inputs, perm=(1, 0, 2, 3, 4))
+
+      x_list = []
+      for i in range(9):
+       
+        # conv layer and lrn layers
+        x = self.conv_layer_1(inputs[i])
+        x = tf.nn.local_response_normalization(x, depth_radius=2, alpha=0.0001, beta=0.75, bias=1.0)
+
+        x = self.conv_layer_2(x)
+        x = tf.nn.local_response_normalization(x, depth_radius=2, alpha=0.0001, beta=0.75, bias=1.0)
+
+        x = self.conv_layer_3(x)
+        x = self.conv_layer_4(x)
+        x = self.conv_layer_5(x)
+
+        x = self.flatten(x)
+        x = self.linear_layer_1(x)
+
+        x_list.append(x)
+
+      # linear layers
+      x = tf.concat(x_list, axis=1)
+      x = self.linear_layer_2(x)
+
+      # output layer
+      out = self.output(x)
+
+      return out 
