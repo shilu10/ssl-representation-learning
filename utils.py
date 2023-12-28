8b24cbc26fca7f4f355c5a821970f528d08ccc95
Shilu <shilu842003@gmail.com>
2023-12-28 07:53:19 +0530

    feat: added context prediction dataloader

--- utils.py
+++ utils.py
@@ -246,7 +246,108 @@ class RotateNetDataLoader(object):
 
         dataset = dataset.interleave(self.parse_file, num_parallel_calls=AUTO)
         dataset = dataset.map(lambda x, y:  tf.py_function(self.preprocess_image, [x, y], [tf.float32, tf.int32]))
+        dataset = dataset.batch()
+        dataset = dataset.prefetch(AUTO)
 
+        return dataset
+
+
+class RotateNetDataLoader(object):
+    def __init__(self, 
+                image_files_path, 
+                rotations=[0, 90, 180, 270], 
+                use_all_rotations=False, 
+                split_type='train',
+                shuffle=True):
+
+        self.image_files_path = image_files_path
+        self.rotations = rotations
+        self.use_all_rotations = use_all_rotations
+        self.split_type = split_type
+        self.shuffle = shuffle
+
+    def parse_file(self, image_path, label=None):
+        raw = tf.io.read_file(image_path)
+
+        if label is not None:
+          return tf.data.Dataset.from_tensors((raw, label))
+
+        return tf.data.Dataset.from_tensors(raw)
+
+    def preprocess_image(self, value, rotation_index):
+        shape = tf.image.extract_jpeg_shape(value)
+
+        image = tf.image.decode_jpeg(value)
+
+        # augmentation
+        image = self.augmentation(image)
+        rotation_value = self.rotations[rotation_index]
+
+        # rotate the image
+        transformed_image = tf.image.rot90(image, k=rotation_index)
+
+        return transformed_image, rotation_index
+
+    def augmentation(self, image):
+        image = tf.image.resize(image, 
+                                size=(256, 256), 
+                                method=tf.image.ResizeMethod.BILINEAR)
+
+        if self.split_type == "train":
+            image = self.center_crop(image, 224, 224)
+
+        else:
+            image = tf.image.random_crop(image, (224, 224))
+            # horizontal flip
+            image = tf.image.random_flip_left_right(image)
+
+        return image
+
+    def center_crop(self, image, crop_height, crop_width):
+        height, width = tf.shape(image)[0], tf.shape(image)[1]
+
+        # Calculate the crop coordinates
+        start_y = (height - crop_height) // 2
+        start_x = (width - crop_width) // 2
+
+        # Perform cropping
+        cropped_image = tf.image.crop_to_bounding_box(
+            image,
+            start_y,
+            start_x,
+            crop_height,
+            crop_width
+        )
+
+        return cropped_image
+
+    def get_dataset(self):
+
+        if self.use_all_rotations:
+            dataset = tf.data.Dataset.from_tensor_slices(tf.convert_to_tensor([], dtype=tf.string))
+            indices = tf.data.Dataset.from_tensor_slices(tf.convert_to_tensor([], dtype=tf.int32))
+
+            dataset = tf.dat.Dataset.zip((dataset, indices))
+
+            for _ in range(len(self.rotations)):
+                inner_dataset = tf.data.Dataset.from_tensor_slices(self.image_files_path)
+                inner_indices = tf.data.Dataset.from_tensor_slices([_ for i in range(len(self.image_files_path))])
+
+                inner_dataset = tf.data.Dataset.zip((inner_dataset, inner_indices))
+
+                dataset = dataset.concatenate(inner_dataset)
+        
+        else:
+            dataset = tf.data.Dataset.from_tensor_slices(self.image_files_path)
+            indices = tf.data.Dataset.from_tensor_slices([random.randint(0, 3) for _ in range(len(self.image_files_path))])
+            dataset = tf.data.Dataset.zip((dataset, indices))
+
+        if self.shuffle:
+            dataset = dataset.shuffle(len(self.image_files_path))
+
+        dataset = dataset.interleave(self.parse_file, num_parallel_calls=AUTO)
+        dataset = dataset.map(lambda x, y:  tf.py_function(self.preprocess_image, [x, y], [tf.float32, tf.int32]))
+        dataset = dataset.batch()
         dataset = dataset.prefetch(AUTO)
 
         return dataset
