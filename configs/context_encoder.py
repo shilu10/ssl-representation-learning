4532f9fc6a997da874bfa982759b6aca0b05666f
Shilu <shilu842003@gmail.com>
2024-01-04 19:21:50 +0530

    feat: updated main_pretext.py and added configs for pretext task

--- configs/context_encoder.py
+++ configs/context_encoder.py
@@ -1,10 +1,8 @@
-batch_size   = 128
 
 config = {}
 
 # params need for model architecture and dataloader
-config['num_classes'] = 10 # just random to maintain consistency in configs
-config['permutation_path'] = 'permutation_max_10.npy'
+
 # model architecture names
 networks = {}
 networks['generator_name'] = 'ContextEncoderGenerator'
@@ -29,8 +27,8 @@ config['optimizer'] = optimizer
 
 # criterion(loss function)
 criterion = {}
-criterion['adversial_type'] = "bce"
-criterion['reconstruction_type'] = "mse"
+criterion['adversial_type'] = "binary_crossentropy"
+criterion['reconstruction_type'] = "mean_squared_error"
 config['criterion'] = criterion
 
 
@@ -45,5 +43,7 @@ model['r_weight'] = 0.999 # recon weight
 model['overlap'] = 0
 model['overlap_weight_multiplier'] = 10
 model['random_masking'] = False
+model['num_classes'] = 10 # just random to maintain consistency in configs
+
 
 config['model'] = model
