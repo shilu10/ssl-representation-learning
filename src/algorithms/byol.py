4213fb594beae372fc2a4e5f4ab0c3f1a0a6f0cc
adminuser <adminuser@kali.mvm>
2024-01-20 17:09:12 +0530

    refactor(all): restructuring of codebase.

--- src/algorithms/byol.py
+++ src/algorithms/byol.py
@@ -4,7 +4,7 @@ import tensorflow as tf
 import tensorflow.keras.backend as K
 from tensorflow.keras.losses import sparse_categorical_crossentropy
 import numpy as np 
-from ..networks import contrastive_task as networks
+from src.networks import contrastive_task as networks
 
 # https://github.com/drkostas?tab=repositories
 
