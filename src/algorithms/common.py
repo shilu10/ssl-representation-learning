154e61fa5efd405ab78315a6460abea7f86e0eca
Shilu <shilu842003@gmail.com>
2024-01-21 21:14:32 +0530

    refactor(algorithms) byol and moco

---
+++ src/algorithms/common.py
@@ -0,0 +1,48 @@
+import tensorflow as tf 
+from tensorflow import keras 
+import numpy as np 
+import os, sys, shutil 
+
+
+class ContrastiveLearning(tf.keras.models.Model):
+	def __init__(self, *args, **kwargs):
+		super(ContrastiveLearning, self).__init__(*args, **kwargs)
+		pass 
+
+	def reset_metrics(self):
+        self.contrastive_accuracy.reset_states()
+        self.correlation_accuracy.reset_states()
+
+	def update_contrastive_accuracy(self, features_1, features_2):
+        features_1 = tf.math.l2_normalize(features_1, axis=1)
+        features_2 = tf.math.l2_normalize(features_2, axis=1)
+        similarities = tf.matmul(features_1, features_2, transpose_b=True)
+
+        batch_size = tf.shape(features_1)[0]
+        contrastive_labels = tf.range(batch_size)
+        self.contrastive_accuracy.update_state(
+            tf.concat([contrastive_labels, contrastive_labels], axis=0),
+            tf.concat([similarities, tf.transpose(similarities)], axis=0),
+        )
+
+    def update_correlation_accuracy(self, features_1, features_2):
+        features_1 = (
+            features_1 - tf.reduce_mean(features_1, axis=0)
+        ) / tf.math.reduce_std(features_1, axis=0)
+        features_2 = (
+            features_2 - tf.reduce_mean(features_2, axis=0)
+        ) / tf.math.reduce_std(features_2, axis=0)
+
+        batch_size = tf.shape(features_1)[0]
+        batch_size = tf.cast(batch_size, dtype=tf.float32)
+        
+        cross_correlation = (
+            tf.matmul(features_1, features_2, transpose_a=True) / batch_size
+        )
+
+        feature_dim = tf.shape(features_1)[1]
+        correlation_labels = tf.range(feature_dim)
+        self.correlation_accuracy.update_state(
+            tf.concat([correlation_labels, correlation_labels], axis=0),
+            tf.concat([cross_correlation, tf.transpose(cross_correlation)], axis=0),
+        )
--- src/algorithms/moco.py
+++ src/algorithms/moco.py
@@ -5,7 +5,7 @@ import tensorflow.keras.backend as K
 from tensorflow.keras.losses import sparse_categorical_crossentropy
 import numpy as np 
 from src.networks import contrastive_task as networks
-
+from .common import ContrastiveLearning
 
 
 def _dense(**custom_kwargs):
@@ -15,7 +15,7 @@ def _dense(**custom_kwargs):
     return _func
 
 
-class MoCo(tf.keras.models.Model):
+class MoCo(ContrastiveLearning):
     """Momentum Contrastive Feature Learning"""
     def __init__(self, config, **kwargs):
 
@@ -111,43 +111,6 @@ class MoCo(tf.keras.models.Model):
         self.optimizer = optimizer
         self.loss = loss
         self.acc_metrics = metrics
-
-    def reset_metrics(self):
-        self.contrastive_accuracy.reset_states()
-        self.correlation_accuracy.reset_states()
-
-    def update_contrastive_accuracy(self, features_1, features_2):
-        features_1 = tf.math.l2_normalize(features_1, axis=1)
-        features_2 = tf.math.l2_normalize(features_2, axis=1)
-        similarities = tf.matmul(features_1, features_2, transpose_b=True)
-
-        batch_size = tf.shape(features_1)[0]
-        contrastive_labels = tf.range(batch_size)
-        self.contrastive_accuracy.update_state(
-            tf.concat([contrastive_labels, contrastive_labels], axis=0),
-            tf.concat([similarities, tf.transpose(similarities)], axis=0),
-        )
-
-    def update_correlation_accuracy(self, features_1, features_2):
-        features_1 = (
-            features_1 - tf.reduce_mean(features_1, axis=0)
-        ) / tf.math.reduce_std(features_1, axis=0)
-        features_2 = (
-            features_2 - tf.reduce_mean(features_2, axis=0)
-        ) / tf.math.reduce_std(features_2, axis=0)
-
-        batch_size = tf.shape(features_1)[0]
-        batch_size = tf.cast(batch_size, dtype=tf.float32)
-        cross_correlation = (
-            tf.matmul(features_1, features_2, transpose_a=True) / batch_size
-        )
-
-        feature_dim = tf.shape(features_1)[1]
-        correlation_labels = tf.range(feature_dim)
-        self.correlation_accuracy.update_state(
-            tf.concat([correlation_labels, correlation_labels], axis=0),
-            tf.concat([cross_correlation, tf.transpose(cross_correlation)], axis=0),
-        )
 
     def train_step(self, inputs):
         
